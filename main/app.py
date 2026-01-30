b8965d8ef5a769bde2b2ff14c8659e118ac20ee9
dalenin <dalenin@openintegration.inc>
2026-01-30 16:34:06 +0300

    feat: test

--- main/app.py
+++ main/app.py
@@ -3,4 +3,4 @@ import uvicorn
 from main.git.github_app_webhooks import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
